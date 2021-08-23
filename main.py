dd12299f4f3feb0f233a5b20b07041d041c19944
Pratik Bhadane <pratikbhadane24@gmail.com>
2021-08-24 00:03:56 +0530

    card flipping with timer

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from os import read
 from tkinter import *
 import pandas
 from random import choice
@@ -7,11 +6,22 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 data = pandas.read_csv("data/french_words.csv")
 words_to_learn = data.to_dict(orient="records")
+current_card = {}
 
 def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = choice(words_to_learn)
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=current_card["French"])
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_bg, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_bg, image=card_back_img)
+
 
 # Screen and Canvas
 window = Tk()
@@ -19,12 +29,15 @@ window.title("Learn with Flash Cards")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 window.resizable(0, 0)
 
+flip_timer = window.after(3000, func=flip_card)
+
 canvas = Canvas(height=526, width=800,)
 card_front_img = PhotoImage(file="./images/card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
+card_back_img = PhotoImage(file="./images/card_back.png")
+card_bg = canvas.create_image(400, 263, image=card_front_img)
 button = Button(image=card_front_img, highlightthickness=0)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-card_title = canvas.create_text(400, 150, text="", font=("Fira Code", 40, "italic"))
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="", font=("Fira Code", 60, "bold"))
 canvas.grid(row=0, column=0, columnspan=2)
 
